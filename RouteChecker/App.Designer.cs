7ae441c90636a25f1ebcd67025057625111a16d7
rickbowden <rick@areyoulocal.me.uk>
2017-04-27 11:37:58 +0100

    Removed test IPs

--- RouteChecker/App.Designer.cs
+++ RouteChecker/App.Designer.cs
@@ -29,26 +29,26 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle21 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle22 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle23 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle24 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle25 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle26 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle27 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle28 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle29 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle30 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle31 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle32 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle33 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle34 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle35 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle36 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle37 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle38 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle39 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle40 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle41 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle42 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle43 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle44 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle45 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle46 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle47 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle48 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle49 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle50 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle51 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle52 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle53 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle54 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle55 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle56 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle57 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle58 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle59 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle60 = new System.Windows.Forms.DataGridViewCellStyle();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -312,7 +312,6 @@
             this.Source_TB.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.Source_TB.Size = new System.Drawing.Size(184, 112);
             this.Source_TB.TabIndex = 19;
-            this.Source_TB.Text = "172.23.36.46";
             this.toolTip1.SetToolTip(this.Source_TB, "IP address or instance name");
             this.Source_TB.WordWrap = false;
             // 
@@ -327,7 +326,6 @@
             this.Dest_TB.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.Dest_TB.Size = new System.Drawing.Size(178, 112);
             this.Dest_TB.TabIndex = 20;
-            this.Dest_TB.Text = "172.23.36.82";
             this.toolTip1.SetToolTip(this.Dest_TB, "IP address or instance name");
             this.Dest_TB.WordWrap = false;
             // 
@@ -342,7 +340,6 @@
             this.Port_TB.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.Port_TB.Size = new System.Drawing.Size(182, 112);
             this.Port_TB.TabIndex = 21;
-            this.Port_TB.Text = "16163";
             this.Port_TB.WordWrap = false;
             this.Port_TB.TextChanged += new System.EventHandler(this.Port_TB_TextChanged);
             // 
@@ -505,71 +502,71 @@
             // 
             // Source_Out
             // 
-            dataGridViewCellStyle21.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.Source_Out.DefaultCellStyle = dataGridViewCellStyle21;
+            dataGridViewCellStyle41.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.Source_Out.DefaultCellStyle = dataGridViewCellStyle41;
             this.Source_Out.HeaderText = "Source";
             this.Source_Out.Name = "Source_Out";
             // 
             // SourceIP_Out
             // 
-            dataGridViewCellStyle22.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.SourceIP_Out.DefaultCellStyle = dataGridViewCellStyle22;
+            dataGridViewCellStyle42.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.SourceIP_Out.DefaultCellStyle = dataGridViewCellStyle42;
             this.SourceIP_Out.HeaderText = "Source IP";
             this.SourceIP_Out.Name = "SourceIP_Out";
             // 
             // SourceSG_Out
             // 
-            dataGridViewCellStyle23.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.SourceSG_Out.DefaultCellStyle = dataGridViewCellStyle23;
+            dataGridViewCellStyle43.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.SourceSG_Out.DefaultCellStyle = dataGridViewCellStyle43;
             this.SourceSG_Out.HeaderText = "SG";
             this.SourceSG_Out.Name = "SourceSG_Out";
             // 
             // SourceProtocol_Out
             // 
-            dataGridViewCellStyle24.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.SourceProtocol_Out.DefaultCellStyle = dataGridViewCellStyle24;
+            dataGridViewCellStyle44.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.SourceProtocol_Out.DefaultCellStyle = dataGridViewCellStyle44;
             this.SourceProtocol_Out.HeaderText = "SG Protcol";
             this.SourceProtocol_Out.Name = "SourceProtocol_Out";
             // 
             // SourcePortRange_Out
             // 
-            dataGridViewCellStyle25.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.SourcePortRange_Out.DefaultCellStyle = dataGridViewCellStyle25;
+            dataGridViewCellStyle45.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.SourcePortRange_Out.DefaultCellStyle = dataGridViewCellStyle45;
             this.SourcePortRange_Out.HeaderText = "SG Port Range";
             this.SourcePortRange_Out.Name = "SourcePortRange_Out";
             // 
             // SourceCidr_Out
             // 
-            dataGridViewCellStyle26.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.SourceCidr_Out.DefaultCellStyle = dataGridViewCellStyle26;
+            dataGridViewCellStyle46.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.SourceCidr_Out.DefaultCellStyle = dataGridViewCellStyle46;
             this.SourceCidr_Out.HeaderText = "SG Source";
             this.SourceCidr_Out.Name = "SourceCidr_Out";
             // 
             // Protocol_Out
             // 
-            dataGridViewCellStyle27.BackColor = System.Drawing.Color.White;
-            this.Protocol_Out.DefaultCellStyle = dataGridViewCellStyle27;
+            dataGridViewCellStyle47.BackColor = System.Drawing.Color.White;
+            this.Protocol_Out.DefaultCellStyle = dataGridViewCellStyle47;
             this.Protocol_Out.HeaderText = "Protocol";
             this.Protocol_Out.Name = "Protocol_Out";
             // 
             // Port_Out
             // 
-            dataGridViewCellStyle28.BackColor = System.Drawing.Color.White;
-            this.Port_Out.DefaultCellStyle = dataGridViewCellStyle28;
+            dataGridViewCellStyle48.BackColor = System.Drawing.Color.White;
+            this.Port_Out.DefaultCellStyle = dataGridViewCellStyle48;
             this.Port_Out.HeaderText = "Port";
             this.Port_Out.Name = "Port_Out";
             // 
             // Dest_Out
             // 
-            dataGridViewCellStyle29.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.Dest_Out.DefaultCellStyle = dataGridViewCellStyle29;
+            dataGridViewCellStyle49.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.Dest_Out.DefaultCellStyle = dataGridViewCellStyle49;
             this.Dest_Out.HeaderText = "Destination";
             this.Dest_Out.Name = "Dest_Out";
             // 
             // DestIP_Out
             // 
-            dataGridViewCellStyle30.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.DestIP_Out.DefaultCellStyle = dataGridViewCellStyle30;
+            dataGridViewCellStyle50.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.DestIP_Out.DefaultCellStyle = dataGridViewCellStyle50;
             this.DestIP_Out.HeaderText = "Destination IP";
             this.DestIP_Out.Name = "DestIP_Out";
             // 
@@ -580,71 +577,71 @@
             // 
             // Source_In
             // 
-            dataGridViewCellStyle31.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.Source_In.DefaultCellStyle = dataGridViewCellStyle31;
+            dataGridViewCellStyle51.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.Source_In.DefaultCellStyle = dataGridViewCellStyle51;
             this.Source_In.HeaderText = "Source";
             this.Source_In.Name = "Source_In";
             // 
             // SourceIP_In
             // 
-            dataGridViewCellStyle32.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.SourceIP_In.DefaultCellStyle = dataGridViewCellStyle32;
+            dataGridViewCellStyle52.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.SourceIP_In.DefaultCellStyle = dataGridViewCellStyle52;
             this.SourceIP_In.HeaderText = "Source IP";
             this.SourceIP_In.Name = "SourceIP_In";
             // 
             // Protocol_In
             // 
-            dataGridViewCellStyle33.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.Protocol_In.DefaultCellStyle = dataGridViewCellStyle33;
+            dataGridViewCellStyle53.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.Protocol_In.DefaultCellStyle = dataGridViewCellStyle53;
             this.Protocol_In.HeaderText = "Protocol";
             this.Protocol_In.Name = "Protocol_In";
             // 
             // Port_In
             // 
-            dataGridViewCellStyle34.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.Port_In.DefaultCellStyle = dataGridViewCellStyle34;
+            dataGridViewCellStyle54.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.Port_In.DefaultCellStyle = dataGridViewCellStyle54;
             this.Port_In.HeaderText = "Port";
             this.Port_In.Name = "Port_In";
             // 
             // Dest_In
             // 
-            dataGridViewCellStyle35.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.Dest_In.DefaultCellStyle = dataGridViewCellStyle35;
+            dataGridViewCellStyle55.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.Dest_In.DefaultCellStyle = dataGridViewCellStyle55;
             this.Dest_In.HeaderText = "Destination";
             this.Dest_In.Name = "Dest_In";
             // 
             // DestIP_In
             // 
-            dataGridViewCellStyle36.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.DestIP_In.DefaultCellStyle = dataGridViewCellStyle36;
+            dataGridViewCellStyle56.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.DestIP_In.DefaultCellStyle = dataGridViewCellStyle56;
             this.DestIP_In.HeaderText = "Destination IP";
             this.DestIP_In.Name = "DestIP_In";
             // 
             // DestSG_In
             // 
-            dataGridViewCellStyle37.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.DestSG_In.DefaultCellStyle = dataGridViewCellStyle37;
+            dataGridViewCellStyle57.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.DestSG_In.DefaultCellStyle = dataGridViewCellStyle57;
             this.DestSG_In.HeaderText = "SG";
             this.DestSG_In.Name = "DestSG_In";
             // 
             // DestProtocol_In
             // 
-            dataGridViewCellStyle38.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.DestProtocol_In.DefaultCellStyle = dataGridViewCellStyle38;
+            dataGridViewCellStyle58.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.DestProtocol_In.DefaultCellStyle = dataGridViewCellStyle58;
             this.DestProtocol_In.HeaderText = "SG Protocol";
             this.DestProtocol_In.Name = "DestProtocol_In";
             // 
             // DestPortRange_In
             // 
-            dataGridViewCellStyle39.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.DestPortRange_In.DefaultCellStyle = dataGridViewCellStyle39;
+            dataGridViewCellStyle59.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.DestPortRange_In.DefaultCellStyle = dataGridViewCellStyle59;
             this.DestPortRange_In.HeaderText = "SG Port Range";
             this.DestPortRange_In.Name = "DestPortRange_In";
             // 
             // DestCIDR_In
             // 
-            dataGridViewCellStyle40.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.DestCIDR_In.DefaultCellStyle = dataGridViewCellStyle40;
+            dataGridViewCellStyle60.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.DestCIDR_In.DefaultCellStyle = dataGridViewCellStyle60;
             this.DestCIDR_In.HeaderText = "SG Source";
             this.DestCIDR_In.Name = "DestCIDR_In";
             // 
